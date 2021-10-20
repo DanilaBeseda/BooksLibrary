efd8880be6d2a0afbe68ccb950191d736b08bbd4
Danila Beseda <danila.beseda1999@gmail.com>
2021-10-20 17:00:33 +0300

    Title to lower case

--- src/store/actions/search.ts
+++ src/store/actions/search.ts
@@ -2,5 +2,5 @@ import { SearchActionTypes } from "../../types/search";
 
 export const changeBookTitle = (title: string) => ({
    type: SearchActionTypes.CHANGE_BOOK_TITLE,
-   payload: title
+   payload: title.toLowerCase()
 })
